eac554a72a517316b157a6b6d36af15557541aaf
Ricardo Caca <ricardo.antonio.caca@gmail.com>
2025-06-28 18:31:52 -0300

    versaoCaca1.0
    core/models criado UsuarioBase do django
    core/templates criado master(base) e home(base)
    criado o app empresa
    empresa/models criado modelo(base)
    empresa/templates criado tela de cadastro(base)criado o app sistema/commands
    implementado o comando limpa_banco, inicializa_sistema

--- canastra/config/urls.py
+++ canastra/config/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('', include('core.urls')),  # sua app aqui
+    path('empresa/', include('empresa.urls')),  # sua app aqui
+
 ]
---
+++ canastra/sistema/management/commands/limpa_banco.py
@@ -0,0 +1,42 @@
+from django.core.management import BaseCommand
+from django.db import connection, transaction
+
+class Command(BaseCommand):
+    help = "Redefine o sistema, apagando tudo do banco"
+
+    @transaction.atomic
+    def handle(self, *args, **options):
+        cursor = connection.cursor()
+
+        app_label = ["core"]
+        tables = [
+            table
+            for table in connection.introspection.table_names()
+            if any(app in table for app in app_label)
+        ]
+
+        if connection.vendor == "postgresql":
+            for table in tables:
+                cursor.execute(f'ALTER TABLE "{table}"DISABLE TRIGGER ALL')
+
+        for table in tables:
+            cursor.execute(f'TRUNCATE TABLE "{table}" CASCADE')
+
+        if connection.vendor == "postgresql":
+            for table in tables:
+                cursor.execute(
+                    f"""
+                    SELECT setval(pg_get_serial_sequence('"{table}"', 'id'), 1, false)
+                        WHERE EXISTS (
+                        SELECT 1 FROM information_schema.columns
+                        WHERE table_name = '"{table}"' AND column_name = 'id'
+                        AND column_default LIKE 'nextval%'
+                    );
+                """
+                )
+
+        if connection.vendor == "postgresql":
+            for table in tables:
+                cursor.execute(f'ALTER TABLE "{table}" ENABLE TRIGGER ALL;')
+
+        self.stdout.write(self.style.SUCCESS("Banco de dados apagado com sucesso!"))
