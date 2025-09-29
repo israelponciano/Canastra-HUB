5fa214a8e70795b5ba2fd6aae19edcf249c2c45a
Ricardo Caca <ricardo.antonio.caca@gmail.com>
2025-09-29 17:59:29 -0300

    master imagem perfil

--- core/views.py
+++ core/views.py
@@ -21,48 +21,39 @@ def cadastro_usuario(request):
     return render(request, 'cadastro_usuario.html')
 
 def login(request):
-    print("CHEGOU LOGIN")
     if request.method == 'POST':
-        print("Chegou POST")
         email = request.POST.get('txtEmail')
         senha = request.POST.get('txtSenha')
-        # perfil_id = request.POST.get('slcPerfil')
 
         print(email, senha)
 
         usuario = authenticate(request, username=email, password=senha)
         print(usuario)
-        # if usuario is not None and perfil_id:
+        
         if usuario is not None:
-            # perfis_usuario = usuario.perfis.filter(id=perfil_id)
-            # if perfis_usuario.exists():
-                #limpa as sessoes do usuario
-                request.session.flush()
-                #cria a sessao do usuario
-                auth_login(request, usuario)
+            request.session.flush()
+            #cria a sessao do usuario
+            auth_login(request, usuario)
                 
-                request.session['is_login'] = False
-                if usuario.is_admin:
-                    request.session['is_admin'] = usuario.is_admin
-                # print(request.session['is_admin'])
-                request.session['perfil'] = usuario.tipo
-                request.session['id_atual'] = usuario.id
-                request.session['email_atual'] = usuario.email                    
-                # request.session['perfil_atual'] = perfis_usuario.first().nome                
-                # request.session['perfis'] = list(usuario.perfis.values_list('nome', flat=True))
-                # print(request.session['perfis'])
-                
-                #configura sessao para expirar em 4 horas
-                request.session.set_expiry(14400)
-                
-                messages.success(request, 'Login realizado com sucesso!')
-                
-                #no futuro iremos separar em diferentes paginas
-                # if request.session.get('perfil_atual') in {'Administrador', 'Funcionario'}:
-                return redirect('core:home')
-                
-            # else:
-            #     messages.error(request, 'Perfil inválido para este usuário.')
+            request.session['is_login'] = False
+            if usuario.foto and hasattr(usuario.foto, "url"):
+                foto = usuario.foto.url
+            else:
+                foto = None
+            if usuario.is_admin:
+                request.session['is_admin'] = usuario.is_admin
+            request.session['nome'] = usuario.nome
+            request.session['foto'] = foto
+            request.session['perfil'] = usuario.tipo
+            request.session['id_atual'] = usuario.id
+            request.session['email_atual'] = usuario.email
+            
+            #configura sessao para expirar em 4 horas
+            request.session.set_expiry(14400)
+            
+            messages.success(request, 'Login realizado com sucesso!')
+            return redirect('core:home')
+        
         else:
             print("Usuario ou senha invalidos")
             messages.error(request, 'Usuário ou senha inválidos.')
