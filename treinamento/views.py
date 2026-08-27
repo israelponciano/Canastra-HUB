04d9f433d82588516b8320e46f0beccc02fd9e54
Jhenefie Sousa <135014192+Jhenefie@users.noreply.github.com>
2026-08-27 10:39:50 -0300

    feat(treinamento): registra log de criação de treinamento
    - Adiciona registrar_log() em criar_treinamento
    - Usa o tipo LogAcao.TipoAcao.TREINAMENTO_CRIADO

--- treinamento/views.py
+++ treinamento/views.py
@@ -5,7 +5,8 @@ from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.http import HttpResponse
 from django.db import models as db_models
-from core.models import Hub
+from core.models import Hub, LogAcao
+from core.utils import registrar_log
 from .models import Treinamento, SessaoTreinamento, InscricaoTreinamento, ListaEspera
 
 
